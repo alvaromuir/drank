22ca3b8e22a1622fdd03a51aa0298e5dfeba0df4
Alvaro Muir <alvaromuir@gmail.com>
2013-12-21 01:30:15 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,42 @@
-drank
-=====
+## Drank
+### Kinda sorta  a fuller baked Flask dev enviroment
+----
+## what is Drank?
+> Drank is flask, with a NPM/BOWER chaser. Opinionated as hell.
 
-Drank is Flask, with a NPM/BOWER chaser.
+----
+## What you need to know
+
+### app structure
+```
+.
+|- app
+     |-- models
+     |-- static
+     |-- templates
+     |-- views
+  |-- ui
+     |-- coffee
+     |-- sass
+  |-- tests
+```
+---
+
+### setup:
+```
+  $ npm install && bower install
+  $ pip install -r requirements.txt
+  $ python drank.py devserver
+  $ ./db_create.py
+  $ ./db_migrate.py
+```
+
+Alvaro Muir, @alvaromuir
+```
+
+Here's the deal:
+NPM sets up grunt and related packages. That includes COMPASS, which requires RUBY (*shakes fist).
+Bower sets up frontend stuff, like jquery, moment, socket-io, etc. You can add more as you need.
+Grunt compiles coffeescript and scss files to /app/static. $ grunt build will concat, minify and uglify files for production.
+
+
---
+++ seed_data.py
@@ -0,0 +1,195 @@
+USERS = {
+    'beyonce': {
+        'social': {
+            'twitter': {
+                'network':'twitter',
+                'network_id': '123456789t',
+                'username': 'beyonce',
+                'network_url': 'https://twitter.com/beyonce'
+            },
+        },
+        'account': {
+            'email': 'beyonce@pullcommitpush.com',
+            'display_name': 'Beyonce Carter',
+            'handle': 'beyonce',
+            'image_url': '/static/img/dev/avatars/beyonce.jpg',
+            'status': 'Who runs the world? Girls.'
+        }
+    },
+    'pbo': {
+        'social': {
+            'twitter': {
+                'network':'twitter',
+                'network_id': '223456789t',
+                'username': 'barackobama',
+                'network_url': 'https://twitter.com/barackobama'
+            },
+            'linkedn': {
+                'network':'linkedin',
+                'network_id': '223456789l',
+                'username': 'barackobama',
+                'network_url': 'https://linkedin.com/in/barackobama'
+            }
+        },
+        'account': {
+            'email': 'obama@pullcommitpush.com',
+            'display_name': 'Barack Obama',
+            'handle': 'obama',
+            'image_url': '/static/img/dev/avatars/pbo.jpg',
+            'status': 'Change, you can believe in.'
+        }
+    },
+    'jennifer': {
+        'social': {
+            'twitter': {
+                'network':'twitter',
+                'network_id': '323456789t',
+                'username': 'jlo',
+                'network_url': 'https://twitter.com/jlo'
+            }
+        },
+        'account': {
+            'email': 'jlo@pullcommitpush.com',
+            'display_name': 'Jennifer Lopez',
+            'handle': 'jennifer',
+            'image_url': '/static/img/dev/avatars/jennifer.jpg',
+            'status': 'Let\'s get loud.'
+        }
+    },
+    'justin': {
+        'social': {
+            'twitter': {
+                'network':'twitter',
+                'network_id': '423456789t',
+                'username': 'jtimberlake',
+                'network_url': 'https://twitter.com/jtimberlake'
+            }
+        },
+        'account': {
+            'email': 'justin@pullcommitpush.com',
+            'display_name': 'Justin Timberlake',
+            'handle': 'justin',
+            'image_url': '/static/img/dev/avatars/justin.jpg',
+            'status': 'CEO, the NEW myspace.com'
+        }
+    },
+    'kanye': {
+        'social': {
+            'twitter': {
+                'network':'twitter',
+                'network_id': '523456789t',
+                'username': 'kanyewest',
+                'network_url': 'https://twitter.com/kanyewest'
+            }
+        },
+        'account': {
+            'email': 'kanye@pullcommitpush.com',
+            'display_name': 'Kanye West',
+            'handle': 'yeezy',
+            'image_url': '/static/img/dev/avatars/kanye.jpg',
+            'status': 'I\'m the God of all things...'
+        }
+    },
+    'kevin': {
+        'social': {
+            'twitter': {
+                'network':'twitter',
+                'network_id': '623456789t',
+                'username': 'KevinHart4real',
+                'network_url': 'https://twitter.com/KevinHart4real'
+            }
+        },
+        'account': {
+            'email': 'kevin@pullcommitpush.com',
+            'display_name': 'Kevin Hart',
+            'handle': 'kevin',
+            'image_url': '/static/img/dev/avatars/kevin.jpg',
+            'status': 'Say it witcha chest'
+        }
+    },
+    'lebron': {
+        'social': {
+            'twitter': {
+                'network':'twitter',
+                'network_id': '723456789t',
+                'username': 'KingJames',
+                'network_url': 'https://twitter.com/KingJames'
+            }
+        },
+        'account': {
+            'email': 'lebron@pullcommitpush.com',
+            'display_name': 'LeBron James',
+            'handle': 'kingjames23',
+            'image_url': '/static/img/dev/avatars/lebron.jpg',
+            'status': 'Witness greatness'
+        }
+    },
+    'questlove': {
+        'social': {
+            'twitter': {
+                'network':'twitter',
+                'network_id': '823456789t',
+                'username': 'questlove',
+                'network_url': 'https://twitter.com/questlove'
+            }
+        },
+        'account': {
+            'email': 'questlove@pullcommitpush.com',
+            'display_name': 'Ahmir Thompson',
+            'handle': 'questlove',
+            'image_url': '/static/img/dev/avatars/questlove.jpg',
+            'status': 'Little drumma boy'
+        }
+    },
+    'rihanna': {
+        'social': {
+            'twitter': {
+                'network':'twitter',
+                'network_id': '823456789t',
+                'username': 'rihanna',
+                'network_url': 'https://twitter.com/rihanna'
+            }
+        },
+        'account': {
+            'email': 'rihanna@pullcommitpush.com',
+            'display_name': 'Robin Fenty',
+            'handle': 'rihanna',
+            'image_url': '/static/img/dev/avatars/rihanna.jpg',
+            'status': 'Bad gyal ri-ri'
+        }
+    },
+    'spike': {
+        'social': {
+            'twitter': {
+                'network':'twitter',
+                'network_id': '823456789t',
+                'username': 'SpikeLee',
+                'network_url': 'https://twitter.com/SpikeLee'
+            }
+        },
+        'account': {
+            'email': 'spikelee@pullcommitpush.com',
+            'display_name': 'Spike Lee',
+            'handle': 'spikelee',
+            'image_url': '/static/img/dev/avatars/spike.jpg',
+            'status': 'Always doing the right thang'
+        }
+    },
+    'tyson': {
+        'social': {
+            'twitter': {
+                'network':'twitter',
+                'network_id': '823456789t',
+                'username': 'MikeTyson',
+                'network_url': 'https://twitter.com/MikeTyson'
+            }
+        },
+        'account': {
+            'email': 'tyson@pullcommitpush.com',
+            'display_name': 'Mike Tyson',
+            'handle': 'ironmike',
+            'image_url': '/static/img/dev/avatars/tyson.jpg',
+            'status': 'Undesputed heavyweight champ of the world'
+        }
+    }
+}
